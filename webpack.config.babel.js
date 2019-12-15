22a7b57ec56b7be0f74be9ef01cf92716ccf5e38
Michiel Peters <wishjuh@gmail.com>
2019-12-15 14:28:20 +0100

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,27 @@
+import path from 'path';
+
+export default {
+  entry: ['./browser.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  devtool: 'source-map',
+};
